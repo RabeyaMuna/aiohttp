8ddd2b7048ae9b1617236213afa505a234e4dfa9
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-12 14:21:39 -0400

    RabeyaMuna__diff__id_351

--- aiohttp/web_urldispatcher.py
+++ aiohttp/web_urldispatcher.py
@@ -213,7 +213,6 @@ class AbstractRoute(abc.ABC):
 
 
 class UrlMappingMatchInfo(BaseDict, AbstractMatchInfo):
-
     __slots__ = ("_route", "_apps", "_current_app", "_frozen")
 
     def __init__(self, match_dict: dict[str, str], route: AbstractRoute) -> None:
@@ -276,7 +275,6 @@ class UrlMappingMatchInfo(BaseDict, AbstractMatchInfo):
 
 
 class MatchInfoError(UrlMappingMatchInfo):
-
     __slots__ = ("_exception",)
 
     def __init__(self, http_exception: HTTPException) -> None:
@@ -731,7 +729,8 @@ class PrefixedSubAppResource(PrefixResource):
 
     async def resolve(self, request: Request) -> _Resolve:
         match_info = await self._app.router.resolve(request)
-        match_info.add_app(self._app)
+        if hasattr(match_info, "add_app"):
+            match_info.add_app(self._app)
         if isinstance(match_info.http_exception, HTTPMethodNotAllowed):
             methods = match_info.http_exception.allowed_methods
         else:
@@ -786,7 +785,7 @@ class Domain(AbstractRuleMatching):
         assert url.raw_host is not None
         if not all(self.re_part.fullmatch(x) for x in url.raw_host.split(".")):
             raise ValueError("Domain not valid")
-        if url.port == 80:
+        if url.port in (None, 80):
             return url.raw_host
         return f"{url.raw_host}:{url.port}"
 
@@ -837,7 +836,8 @@ class MatchedSubAppResource(PrefixedSubAppResource):
         if not await self._rule.match(request):
             return None, set()
         match_info = await self._app.router.resolve(request)
-        match_info.add_app(self._app)
+        if hasattr(match_info, "add_app"):
+            match_info.add_app(self._app)
         if isinstance(match_info.http_exception, HTTPMethodNotAllowed):
             methods = match_info.http_exception.allowed_methods
         else:
